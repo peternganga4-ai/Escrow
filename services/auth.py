112e6a4141398e00735794fc7f9aa805eed16482
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-09-10 23:11:16 +0300

    added has_role method,login auth message display

--- services/auth.py
+++ services/auth.py
@@ -18,9 +18,35 @@ class AuthManager:
         self.current_user = user
         print(f"\n Welcome, {user.name}! Role: {user.role}")
         return user
-
+#logout method $ display message with the current user name
     def logout(self):
-         if
+         if self.current_user:
+              print(f"\n Goodbye, {self.current_user.name}!")
+              self.current_user = None
+#Actual check for login user
+    def is_authenticated(self):
+         return self.current_user is not None
+
+#creates method $ gives the roles to auth personel
+    def has_role(self, *roles):
+         return self.is_authenticated() and self.current_user.role in roles
+#defines roles&gives several roles
+    def require_role(self, *roles):
+
+         #checks whether login is auth&display themessage
+         if not self.is_authenticated():
+              print("\n You must be logged in.")
+              return False
+
+         #calls has_role method and unpacks tuple
+         if not self.has_role(*roles):
+              #displays message&joins the roles whether retailer or buyer
+              print(f"\n Access denied.Required: {','.join(roles)}")
+              return False
+         return True
+    
+    
+              
 
     
 
