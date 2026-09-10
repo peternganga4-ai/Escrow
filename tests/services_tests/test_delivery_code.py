c406ca20ac4ab4cc1917aa7b493766553488f299
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-09-10 10:40:31 +0300

    verification of wrong,no code code input

--- tests/services_tests/test_delivery_code.py
+++ tests/services_tests/test_delivery_code.py
@@ -16,4 +16,18 @@ def test_verify_correct_code():
     txn = Transaction("TXN001", "P001", "B001", "R001", 50000, "PAID")
     txn.delivery_code = "654321"
     assert verify_delivery_code(txn, "654321") is True
-       
+def test_verify_wrong_code():
+    txn = Transaction("TXN001", "P001", "B001", "R001", 50000, "PAID")
+    txn.delivery_code = "654321"
+    assert verify_delivery_code(txn, "000000") is False
+
+def test_verify_no_code_on_txn():
+    txn = Transaction("TXN001", "P001", "B001", "R001", 50000)
+    assert txn.delivery_code is None
+    assert verify_delivery_code(txn, "123456") is False
+
+
+def test_verify_none_code_input():
+    txn = Transaction("TXN001", "P001", "B001", "R001", 50000, "PAID")
+    txn.delivery_code = "123456"
+    assert verify_delivery_code(txn, None) is False         
