90fd15f55459dbac2c521b9ebfdd971ff4425dff
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-09-10 10:55:08 +0300

    test for verifying leger enntry&tp_id

--- tests/services_tests/test_ledger.py
+++ tests/services_tests/test_ledger.py
@@ -12,3 +12,10 @@ def test_add_ledger_entry_basic(patched_db):
     assert ledger[0]["amount"] == 50000
     assert ledger[0]["from"] == "B001"
 
+def test_add_ledger_entry_with_to(patched_db):
+    add_ledger_entry("TXN002", "RELEASE", 30000, to_id="R001")
+    ledger = patched_db.load("ledger")
+    assert ledger[0]["to"] == "R001"
+    assert "from" not in ledger[0]
+
+    
