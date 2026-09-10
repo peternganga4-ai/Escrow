6e2a55d640438d44c839d684482adf926971bded
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-09-10 11:44:39 +0300

    verify retailer&delivery starting balances

--- tests/services_tests/test_user_service.py
+++ tests/services_tests/test_user_service.py
@@ -10,3 +10,12 @@ def test_register_buyer_gets_200k(patched_db):
     assert user.balance == 200000
 
 
+def test_register_retailer_gets_100k(patched_db):
+    user = register_user("Store", "store", "p", "RETAILER")
+    assert user.balance == 100000
+
+
+def test_register_delivery_gets_50k(patched_db):
+    user = register_user("Driver", "driver", "p", "DELIVERY")
+    assert user.balance == 50000
+
