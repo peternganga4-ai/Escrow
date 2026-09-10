cd35eb6e1ab1a4218eed2baff4784f150d0a0af3
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-09-10 10:36:32 +0300

    verifying correct code

--- tests/services_tests/test_delivery_code.py
+++ tests/services_tests/test_delivery_code.py
@@ -3,3 +3,17 @@
 from core.transaction_model import Transaction
 from services.delivery_code import generate_delivery_code, verify_delivery_code
 
+def test_generate_code_format():
+    code = generate_delivery_code()
+    assert len(code) == 6
+    assert code.isdigit()
+
+
+def test_generate_code_uniqueness():
+    codes = {generate_delivery_code() for _ in range(20)}
+    assert len(codes) > 1
+def test_verify_correct_code():
+    txn = Transaction("TXN001", "P001", "B001", "R001", 50000, "PAID")
+    txn.delivery_code = "654321"
+    assert verify_delivery_code(txn, "654321") is True
+       
